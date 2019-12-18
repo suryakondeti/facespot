7e84891176366b499766886c091f4fa45b5d6406
Surya Prassad Kondeti <suryapkondeti@gmail.com>
2019-12-18 11:58:17 -0500

    Second

--- face_recognition/settings.py
+++ face_recognition/settings.py
@@ -122,18 +122,3 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-AWS_ACCESS_KEY_ID = 'AKIASJDAEEF4LCF5DQEC'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'facespot.me'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/media'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
